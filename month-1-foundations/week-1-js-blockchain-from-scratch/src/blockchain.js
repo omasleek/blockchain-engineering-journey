874952849dcd610243ee2669cfad7e09add0f7e8
omasleek <polichilda66@gmail.com>
2026-01-05 22:38:26 +0100

    Day 4 complete

--- month-1-foundations/week-1-js-blockchain-from-scratch/src/blockchain.js
+++ month-1-foundations/week-1-js-blockchain-from-scratch/src/blockchain.js
@@ -3,6 +3,7 @@ const Block = require("./block");
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
   }
 
   createGenesisBlock() {
@@ -13,46 +14,20 @@ class Blockchain {
     return this.chain[this.chain.length - 1];
   }
 
-  addBlock(data) {
-    const latestBlock = this.getLatestBlock();
-    const newBlock = new Block(
-      this.chain.length,
-      new Date().toISOString(),
-      data,
-      latestBlock.hash
-    );
-
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
-  }
-  isChainValid() {
-    for (let i = 1; i < this.chain.length; i++) {
-      const currentBlock = this.chain[i];
-      const previousBlock = this.chain[i - 1];
-
-      if (currentBlock.hash !== currentBlock.calculateHash()) {
-        return false;
-      }
-
-      if (currentBlock.previousHash !== previousBlock.hash) {
-        return false;
-      }
-    }
-
-    return true;
   }
 }
 
-// Test my blockchain
-let myCoin = new Blockchain();
+// TESTING THE BLOCKCHAIN
+const myBlockchain = new Blockchain();
 
-myCoin.addBlock({ amount: 50 });
-myCoin.addBlock({ amount: 200 });
+console.log("⛏️ Mining block 1...");
+myBlockchain.addBlock(new Block(1, new Date().toISOString(), { amount: 100 }));
 
-console.log("Is blockchain valid?", myCoin.isChainValid());
+console.log("⛏️ Mining block 2...");
+myBlockchain.addBlock(new Block(2, new Date().toISOString(), { amount: 50 }));
 
-// ATTACK: Someone tampers with block data
-myCoin.chain[1].data = { amount: 1000 };
-
-console.log("Is blockchain valid after attack?", myCoin.isChainValid());
-console.log(JSON.stringify(myCoin, null, 2));
-
+console.log(JSON.stringify(myBlockchain, null, 2));
