4a53dade55e86b1a9130e1895ee97f1b6e04acbe
omasleek <polichilda66@gmail.com>
2026-01-05 22:32:48 +0100

    mining

--- month-1-foundations/week-1-js-blockchain-from-scratch/src/block.js
+++ month-1-foundations/week-1-js-blockchain-from-scratch/src/block.js
@@ -1,30 +1,35 @@
-const crypto = require("crypto");
+const SHA256 = require("crypto-js/sha256");
 
 class Block {
-  constructor(index, timestamp, data, previousHash) {
+  constructor(index, timestamp, data, previousHash = "") {
     this.index = index;
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = previousHash;
+    this.nonce = 0; // number used once
     this.hash = this.calculateHash();
   }
 
   calculateHash() {
-    return crypto
-      .createHash("sha256")
-      .update(
-        this.index +
-          this.previousHash +
-          this.timestamp +
-          JSON.stringify(this.data)
-      )
-      .digest("hex");
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
+    ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log("✅ Block mined:", this.hash);
   }
 }
 
-// Test block
-const block = new Block(1, new Date().toISOString(), { amount: 100 }, "abc123");
-
-console.log(block);
 module.exports = Block;
-
